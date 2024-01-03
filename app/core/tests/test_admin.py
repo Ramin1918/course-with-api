5a7d0aabaf6ef415db6297ee4a79e1adfb362667
ramin <eminli.ramin@icloud.com>
2024-01-03 18:03:54 +0100

    User model and django Admin

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -36,8 +36,6 @@ class AdminSiteTests(TestCase):
         """Test the edit user page works."""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
-
-
         self.assertEqual(res.status_code, 200)
 
     def test_create_user_page(self):
